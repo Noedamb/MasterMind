b028db4324534ad4c57a4c6470ab2fff6dd55757
btssio <btssio@PC-00326-EJA.cite-scolaire.lan>
2023-11-13 17:40:33 +0100

    verification

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -11,19 +12,24 @@ namespace MasterMind
             StringBuilder chaine = new StringBuilder("[-][-][-][-][-]");
 
             Regex regex = new Regex("[brnvjog]");
-            Console.WriteLine("Entrez une lettre : ");
+            Console.WriteLine("Saisir 5 couleur : ");
             int p = 0;
+            char lettre;
 
             do
-            {
-                char lettre = Console.ReadKey().KeyChar;
-                int index = chaine.ToString().IndexOf('-');
-                if (index != -1)
+            { lettre = Console.ReadKey().KeyChar;
+
+                if (regex.IsMatch(lettre.ToString()))
                 {
-                    chaine[index] = lettre;
-                    p++;
+                    int index = chaine.ToString().IndexOf('-');
+                    if (index != -1)
+                    {
+                        chaine = chaine.Replace('-', lettre, index, 1);
+                        p++;
+                    }
                 }
-                Console.Clear();
+
+                Console.Clear(); // clear l'affichage
                 foreach (char c in chaine.ToString())
                 {
                     if (regex.IsMatch(c.ToString()))
@@ -37,24 +43,22 @@ namespace MasterMind
                         Console.Write(c);
                     }
                 }
-
                 Console.WriteLine();
             } while (p < 5);
-
         }
 
         private static ConsoleColor GetConsoleColor(char lettre)
         {
             switch (lettre)
             {
-                case 'b':return ConsoleColor.Blue;
-                case 'r':return ConsoleColor.Red;
-                case 'n':return ConsoleColor.White;
-                case 'v':return ConsoleColor.Green;
-                case 'j':return ConsoleColor.Yellow;
-                case 'o':return ConsoleColor.White;
-                case 'g':return ConsoleColor.Gray;
-                default:return ConsoleColor.White;
+                case 'b': return ConsoleColor.Blue;
+                case 'r': return ConsoleColor.Red;
+                case 'n': return ConsoleColor.White;
+                case 'v': return ConsoleColor.Green;
+                case 'j': return ConsoleColor.Yellow;
+                case 'o': return ConsoleColor.White;
+                case 'g': return ConsoleColor.Gray;
+                default: return ConsoleColor.Black;
             }
         }
     }
