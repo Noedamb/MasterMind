a41906eba62185945d2c24ab954cc1c0ea3304b2
btssio <btssio@PC-00326-EJA.cite-scolaire.lan>
2023-11-27 17:35:48 +0100

    Finalisation Caravalide

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -6,29 +6,32 @@ namespace MasterMind
 {
     class Program
     {
-        public static void Main(string[] args)
+    	public static void Main(string[] args)
+    	{ 
+    		Console.Write("Veuillir utiliser ces couleurs : ");
+			Console.ForegroundColor = ConsoleColor.Blue;Console.Write("Bleu(B) ");Console.ForegroundColor = ConsoleColor.DarkRed;Console.Write("Rouge(R) ");
+			Console.ForegroundColor = ConsoleColor.White;Console.Write("Noir(N) ");Console.ForegroundColor = ConsoleColor.Green;Console.Write("Vert(V) ");
+			Console.ForegroundColor = ConsoleColor.Yellow;Console.Write("Jaune(J) ");Console.ForegroundColor = ConsoleColor.DarkYellow;Console.Write("Orange(O) ");
+			Console.ForegroundColor = ConsoleColor.DarkGray;Console.WriteLine("Gris(G )");Console.ResetColor();
+    		CaraValide(args);}
+        public static void CaraValide(string[] args)
         {
             StringBuilder chaine = new StringBuilder("[-][-][-][-][-]"); // créé une chaine StringBuilder pour configurer une chaîne de caractères
-
             Regex regex = new Regex("[brnvjog]"); // Crée un Regex pour vérifier si des lettres correspondent dans "[brnvjog]".
             Console.WriteLine("Saisir 5 couleur : "); // Affiche un message à l'utilisateur.
             int p = 0; // creer une variable pour compter le nombre de lettres valides saisies par l'utilisateur.
             char lettre; // Déclare une variable pour stocker la lettre saisie par l'utilisateur.
-
             do
-            { 
-                lettre = Console.ReadKey().KeyChar; // saisir par l'utilisateur
-
+            {   lettre = Console.ReadKey().KeyChar; // saisir par l'utilisateur
                 if (regex.IsMatch(lettre.ToString())) // Vérifie si la lettre saisie correspond à l'une des lettres dans "[brnvjog].
                 {
                     int index = chaine.ToString().IndexOf('-'); // l'index recupere les '-' dans la chaine
                     if (index != -1)
                     {
                         chaine = chaine.Replace('-', lettre, index, 1); // Remplace le premier '-' 
-                        p++; // 
+                        p++;
                     }
                 }
-
                 Console.Clear(); 
                 foreach (char c in chaine.ToString()) // Parcourt chaque caractère de la chaîne.
                 {
@@ -44,9 +47,11 @@ namespace MasterMind
                     }
                 }
                 Console.WriteLine(); // Passe à la ligne suivante.
-            } while (p < 5);
+            } while (p < 5);{
+            	Console.Clear(); 
+            	test(args);
+            }
         }
-
         private static ConsoleColor GetConsoleColor(char lettre) 
         {
             switch (lettre) // Utilise une instruction switch pour déterminer la couleur en fonction de la lettre.
@@ -55,11 +60,28 @@ namespace MasterMind
                 case 'r': return ConsoleColor.DarkRed; 
                 case 'n': return ConsoleColor.White;
                 case 'v': return ConsoleColor.Green;
-                case 'j': return ConsoleColor.DarkYellow;
+                case 'j': return ConsoleColor.Yellow;
                 case 'o': return ConsoleColor.DarkYellow;
                 case 'g': return ConsoleColor.DarkGray;
                 default: return ConsoleColor.Black; 
             }
         }
+        public static void test(string[] args)
+        {
+            Console.WriteLine("2ème joueur :                Bien placé                     Mal placé");
+            Console.WriteLine("essaie : [-][-][-][-][-]        {X}                            {X}");
+            Console.ReadLine();    
+
+        }
+			public static void CalculBpMp(ref int bp, int mp, char[] combinaison, char essaie, char[] combinaisonEssaie)
+			{
+			
+			}
+			public static void Joueur2(ref int nbE, char[] combinaison, char[] essaie)
+			{
+			
+			}
     }
+    
 }
+
